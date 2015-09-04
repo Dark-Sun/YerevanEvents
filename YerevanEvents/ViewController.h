0f7ae1c93f1219baf912598f983c2c62ab7d2eab
Andriy Lukashchuk <darksundarksun@gmail.com>
2015-09-04 03:04:14 +0300

    Initial Commit

---
+++ YerevanEvents/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YerevanEvents
+//
+//  Created by Andriy Lukashchuk on 9/4/15.
+//  Copyright (c) 2015 Codegemz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YerevanEvents/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YerevanEvents
+//
+//  Created by Andriy Lukashchuk on 9/4/15.
+//  Copyright (c) 2015 Codegemz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
